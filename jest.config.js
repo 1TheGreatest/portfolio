44827592bde599853872b528072b68f0a2feb2b8
1TheGreatest <soloeben12@gmail.com>
2025-02-06 01:06:19 +0000

    added sentry functionality

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,23 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
-// module.exports = {
-//   testEnvironment: "node",
-//   transform: {
-//     "^.+.tsx?$": ["ts-jest",{}],
-//   },
-// };
+
 module.exports = {
-  roots: ["<rootDir>"], // Change from "<rootDir>/src" to "<rootDir>"
-  testMatch: ["**/?(*.)+(test|spec).[jt]s?(x)"], // Match test files properly
-  coveragePathIgnorePatterns: ["/node_modules/", "/.next/", "/public/"]
+  // Enable coverage collection:
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov", "text"], // lcov is used by SonarQube
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  transform: {
+    "^.+.tsx?$": ["ts-jest", {}],
+  },
+  reporters: ["default"],
+  outputDirectory: "reports", // directory where the report will be saved
+  outputName: "sonar-report.xml", // the filename that SonarQube expects
+
+  testResultsProcessor: "jest-sonar-reporter",
 };
+// module.exports = {
+//   roots: ["<rootDir>"], // Change from "<rootDir>/src" to "<rootDir>"
+//   testMatch: ["**/?(*.)+(test|spec).[jt]s?(x)"], // Match test files properly
+//   coveragePathIgnorePatterns: ["/node_modules/", "/.next/", "/public/"]
+// };
--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -1,14 +1,17 @@
+// Load environment variables from .env file
+require("dotenv").config();
+
 const scanner = require("sonarqube-scanner").default;
 
 scanner(
   {
     serverUrl: "http://localhost:9000",
-    token: "squ_1553dca4875d9faa92f96663378e276e1e76e721",
+    token: process.env.SONAR_TOKEN || "",
     options: {
       "sonar.sources": ".",
       "sonar.eslint.reportPaths": "eslint-report.json",
-      "sonar.exclusions": "**/*.test.tsx",
-      "sonar.tests": ".src", // can be ./tests
+      "sonar.exclusions": "node_modules/**,coverage/**,reports/**",
+      "sonar.tests": ".", // can be ./tests
       "sonar.test.inclusions": "**/*.test.tsx,**/*.test.ts", // can be ./tests/**/*.test.tsx
       "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
       "sonar.testExecutionReportPaths": "test-report.xml",
