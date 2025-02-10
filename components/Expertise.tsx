ea449ae1dad0746f0ec675a7781f9cf05de8451d
1TheGreatest <soloeben12@gmail.com>
2025-02-10 01:52:04 +0000

    expertise responsive fixed

--- components/Expertise.tsx
+++ components/Expertise.tsx
@@ -2,29 +2,68 @@ import React from "react";
 import { OrbitingCircles } from "./ui/orbiting-circles";
 import { backend, databases, frontend, others } from "@/data/index";
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Expertise = () => {
+  const [radius1, setRadius1] = useState(80);
+  const [radius2, setRadius2] = useState(140);
+  const [radius3, setRadius3] = useState(200);
+  const [radius4, setRadius4] = useState(260);
+
+  const isBase = useMediaQuery({ minWidth: 0, maxWidth: 639 }); // base
+  const isSmall = useMediaQuery({ minWidth: 640, maxWidth: 767 }); // sm
+  const isMedium = useMediaQuery({ minWidth: 768, maxWidth: 1023 }); // md
+  const isLarge = useMediaQuery({ minWidth: 1024 }); // lg
+
+  useEffect(() => {
+    if (isBase) {
+      setRadius1(20);
+      setRadius2(50);
+      setRadius3(90);
+      setRadius4(130);
+    } else if (isSmall) {
+      setRadius1(40);
+      setRadius2(100);
+      setRadius3(160);
+      setRadius4(220);
+    } else if (isMedium) {
+      setRadius1(60);
+      setRadius2(120);
+      setRadius3(180);
+      setRadius4(240);
+    } else if (isLarge) {
+      setRadius1(80);
+      setRadius2(140);
+      setRadius3(200);
+      setRadius4(260);
+    }
+  }, [isBase, isSmall, isMedium, isLarge]);
+
   return (
     <section
       id="expertise"
-      className="w-full h-[100vh] px-[14vw] py-[11.4vh] bg-white"
+      className="w-full h-[50vh] sm:h-[100vh] px-[10vw] sm:px-[14vw] py-[5vh] sm:py-[11vh] bg-white"
     >
       <div className="relative flex flex-col min-h-full items-center justify-center">
-        <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-black">
+        <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300 bg-clip-text text-center text-xl sm:text-6xl md:text-7xl lg:text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-black">
           Expertise
         </span>
-        <OrbitingCircles iconSize={40} radius={100} speed={0.5}>
+        <OrbitingCircles
+          className="size-4 sm:size-6 md:size-8 lg:size-10"
+          radius={radius1}
+          speed={0.5}
+        >
           {frontend.map((item, idx) => (
-            <Image
-              key={idx}
-              src={item.logo}
-              alt="frontend logo"
-              width={40}
-              height={40}
-            />
+            <Image key={idx} src={item.logo} alt="frontend logo" fill />
           ))}
         </OrbitingCircles>
-        <OrbitingCircles iconSize={40} radius={160} reverse speed={0.5}>
+        <OrbitingCircles
+          className="size-4 sm:size-6 md:size-8 lg:size-10"
+          radius={radius2}
+          reverse
+          speed={0.5}
+        >
           {backend.map((item, idx) => (
             <Image
               key={idx}
@@ -35,7 +74,11 @@ const Expertise = () => {
             />
           ))}
         </OrbitingCircles>
-        <OrbitingCircles iconSize={40} radius={220} speed={0.5}>
+        <OrbitingCircles
+          className="size-4 sm:size-6 md:size-8 lg:size-10"
+          radius={radius3}
+          speed={0.5}
+        >
           {databases.map((item, idx) => (
             <Image
               key={idx}
@@ -46,7 +89,12 @@ const Expertise = () => {
             />
           ))}
         </OrbitingCircles>
-        <OrbitingCircles iconSize={40} radius={280} reverse speed={0.5}>
+        <OrbitingCircles
+          className="size-4 sm:size-6 md:size-8 lg:size-10"
+          radius={radius4}
+          reverse
+          speed={0.5}
+        >
           {others.map((item, idx) => (
             <Image
               key={idx}
--- components/ui/client-logos.tsx
+++ components/ui/client-logos.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const ClientLogo = ({ svg }: { svg: string }) => {
   return (
-    <div className="flex sm:px-3 md:px-4 lg:px-7 sm:py-2 md:py-3 lg:py-4 border border-navcolor rounded-lg items-center justify-center filter grayscale">
+    <div className="flex sm:px-3 md:px-4 lg:px-10 sm:py-2 md:py-3 lg:py-4 border border-navcolor rounded-lg items-center justify-center filter grayscale">
       <div className="relative w-8 h-3 sm:w-12 sm:h-4 md:w-16 md:h-5 lg:w-20 lg:h-6">
         <Image
           src={`/icons/${svg}`}
